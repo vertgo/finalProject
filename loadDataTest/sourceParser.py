240ee19a37f63a9e292338c36324417c54e96753
Mike Manh <github.123@vteklabs.com>
2013-05-30 18:13:49 -0400

    some more basic code to clean and use the data

--- loadDataTest/sourceParser.py
+++ loadDataTest/sourceParser.py
@@ -3,5 +3,35 @@ def getSource( inURL ):
 		return "facebook"
 
 
+	elif "tripadvisor.com" in inURL:
+		return "tripadvisor"
+
+	elif "flickr.com" in inURL:
+		return "flickr"
+
+	elif "yelp.com" in inURL:
+		return "yelp"
+	elif "yelpcdn.com" in inURL:
+		return "yelp"
+
+	elif "tumblr" in inURL:
+		return "tumblr"
+
+	elif "4sqi.net" in inURL:
+		return "4square"
+
+	elif "nytimes.com" in inURL:
+		return "nytimes"
+
+	elif "wp-content" in inURL:
+		return "blog"
+	elif "blogspot" in inURL:
+		return "blog"	
+	elif "wp-content" in inURL:
+		return "blog"
+	elif "wordpress.com" in inURL:
+		return "blog"
+
+
 #assuming df is the dataframe
-df[ 'source' ] = map( getSource,  df[ 'Image_url' ] )
+#df[ 'source' ] = map( getSource,  df[ 'Image_url' ] )
--- processImage/processImage.py
+++ processImage/processImage.py
@@ -1,10 +1,32 @@
 from PIL import Image
 from PIL.ExifTags import TAGS
+import numpy
 
-im = Image.open( "sample.jpg")
+def exifStuff():
+	im = Image.open( "sample.jpg")
 
-exif = {
-    PIL.ExifTags.TAGS[k]: v
-    for k, v in im._getexif().items()
-    if k in TAGS
-}
+	exif = {
+	    PIL.ExifTags.TAGS[k]: v
+	    for k, v in im._getexif().items()
+	    if k in TAGS
+	}
+
+def openImage( inFilename ):
+	try:
+		im = Image.open( "images/" + inFilename )
+		#width, height = im.size
+		return im
+	except:
+		return None
+
+#size is aspect ratio, totalPixels, width, height
+
+def populateSizeFields( inImageName ):
+	try:
+		im = Image.open( "images/" + inImageName )
+		width, height = im.size
+		aspect = float( width) / float( height )
+		pixels = width * height
+		return pixels, aspect
+	except:
+		return numpy.nan, numpy.nan
